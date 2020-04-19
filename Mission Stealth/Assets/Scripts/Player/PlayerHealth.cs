a0b98cd171407d807a5e36dc1e7c5f2ea5867502
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2020-04-19 14:19:50 +0530

    Escape System
    The player will collect the key and escape

---
+++ Mission Stealth/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public float health = 100f;
+	public float resetAfterDeathTime = 5f;
+	public AudioClip deathClip;
+
+	private FirstPersonController playerMovement;
+	private LastPlayerSighting lastPlayerSighting;
+	private float timer;
+	private bool playerDead;
+
+	void Awake()
+	{
+		playerMovement = GetComponent<FirstPersonController>();
+		lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+	}
+
+	void Update()
+	{
+		if(health <= 0f)
+		{
+			if(!playerDead)
+			{
+				PlayerDying();
+			}
+			else
+			{
+				PlayerDead();
+				LevelReset();
+			}
+		}
+	}
+
+	void PlayerDying()
+	{
+		playerDead = true;
+		AudioSource.PlayClipAtPoint(deathClip, transform.position);
+	}
+	void PlayerDead(){
+		playerMovement.enabled = false;
+		lastPlayerSighting.position = lastPlayerSighting.resetPosition;
+	}
+
+	void LevelReset()
+	{
+
+	}
+
+	public void TakeDamage(float amount)
+	{
+		health -= amount;
+	}
+
+}
